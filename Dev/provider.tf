76866dd7e62d286849828ff6b9e51b1adbb4cfcc
Jaydeepc1985 <jaydeep.chakraborty1985@gmail.com>
2025-09-26 22:20:31 +0530

    Update provider.tf

--- Dev/provider.tf
+++ Dev/provider.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
   features {}
   subscription_id = "845670d0-6329-46a7-97a7-fe36dbdce51b"
 }
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "jaydeep_rg"
-#     storage_account_name = "jaydeepstg321"
-#     container_name       = "jaydeepcnt321"
-#     key                  = "terraform.tfstate"
+ terraform {
+   backend "azurerm" {
+     resource_group_name  = "jaydeep_rg"
+     storage_account_name = "jaydeepstg321"
+     container_name       = "jaydeepcnt321"
+     key                  = "terraform.tfstate"
 
-#   }
-#  }
+   }
+  }
